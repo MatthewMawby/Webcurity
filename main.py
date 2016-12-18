1e1a14feccd3a4438ac91b02470265e2eb6b7218
MatthewMawby <themesta@gmail.com>
2016-12-17 20:16:25 -0500

    image saved and emailed upon motion detection

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ import sys
 settings_file = open('settings.json', 'r')
 settings = json.load(settings_file)
 
-
+SECONDS = int(settings['record_time'])
 RES_X = int(settings['xres'])
 RES_Y = int(settings['yres'])
 EMAIL = settings['email']
 NOTIFY = settings['email_notifications']
 MIN_AREA = int(settings['min_contour'])  #Min area of contours to detect as motion
 camera = cv2.VideoCapture(0)        #Initialize the webcam for videocapture
-firstFrame = None                   #Set the firstframe to none
+backFrame = None                   #Set the backFrame to none
 
 # Define the codec for video writing
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -42,9 +42,6 @@ if NOTIFY:
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
-
-#mes = create_message(EMAIL, EMAIL, 'Motion Detected!', "Motion has been detected by your security webcam!")
-#send_message(service, "me", mes)
 
 wait_for_focus = True
 time_detected = None
@@ -67,7 +64,7 @@ while True:
             #Write time and date
             cv2.putText(framecopy, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
             out.write(framecopy)
-        if datetime.datetime.now() >= time_detected + datetime.timedelta(seconds=30):
+        if datetime.datetime.now() >= time_detected + datetime.timedelta(seconds=SECONDS):
             motion_detected = False
             time_detected = None
             out.release()
@@ -77,27 +74,33 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    #set the firstframe if not set
-    if firstFrame is None:
-        firstFrame = gray
+    #set the backFrame if not set
+    if backFrame is None:
+        backFrame = gray
         wait_for_focus = False
         continue
 
     #Find the difference between the current frame and the first frame. Difference = "motion"
-    frameDelta = cv2.absdiff(firstFrame, gray)
+    frameDelta = cv2.absdiff(backFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
 
     #Find all the contours in the current frame difference
     _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    #If any of the contours are larger than the minimum area...
+    #Loop through all contours in the frame differences
     for c in cnts:
+        #If the contour isn't large enough
         if cv2.contourArea(c) < MIN_AREA:
             continue
 
         if not motion_detected and time_detected is None:
             print "Motion Detected!"
+            #Create an image and send it
+            cv2.imwrite(datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p")+".jpg", frame)
+            mes = create_message(EMAIL, EMAIL, 'Motion Detected!', "Motion has been detected by your security webcam!", datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p")+".jpg")
+            send_message(service, "me", mes)
+            #Flag motion_detection as true and begin video recording
             motion_detected = True
             time_detected = datetime.datetime.now()
             fname = datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p")+'_WebCurity.avi'
@@ -112,8 +115,12 @@ while True:
     #Display the frame
     cv2.imshow("Security Feed", frame)
 
-    key = cv2.waitKey(1) & 0xFF
+    #if there's no motion in the frame, set the background to the current frame
+    if not motion_detected:
+        backFrame = gray
 
+    #Press q to quit
+    key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
 
